01a6b446a0a4a8ff85f14cfc1564053730f02381
olnaz <olnazx@gmail.com>
2017-03-22 17:48:27 +0300

    Update gift.js

--- src/bot/parsers/gift.js
+++ src/bot/parsers/gift.js
@@ -1,28 +1,12 @@
 'use strict';
 
 /**
- * Module dependencies.
- * @private
- */
-const User = require('../../database/models/user');
-
-/**
  * Боту прислали подарок.
- * @param {Object}
- *   @property {Number}    id   ID бота
- *   @property {Object}    data Объект сообщения
- *   @property {Reference} app  Ссылка на экземпляр Application
- *   @property {Object}    user Объект пользователя
- * @return {Object/String}
+ * @return {String}
  * @public
  */
-async function gift ({ id, data, app, user }) {
-  let isOk = await User.upd(data.sender_id, { $inc: { points_count: 50 } });
-
-  if (!isOk) 
-    return 'Спасибо за подарок, дружище! :-) :-*';
-  else 
-    return 'Спасибо за подарок, дружище! :-) :-*\n\nДержи 50 поинтов на свой баланс!';
+async function gift () {
+  return 'Спасибо за подарок, дружище! :-) :-*';
 }
 
 module.exports = gift;
