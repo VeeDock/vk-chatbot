57ae868572c26cc714815737290b64f09e6855af
olnaz <olnazx@gmail.com>
2017-04-17 22:25:13 +0300

    www server is rewritten

---
+++ backend/server/server/lib/Router.js
@@ -0,0 +1,46 @@
+'use strict';
+
+class Router {
+  constructor () {
+    this.routes = Object.create(null);
+  }
+
+  /**
+   * Возвращает обработчик маршрута.
+   * @param  {String}   url Маршрут
+   * @return {Function}     Обработчик (undefined, если обработчика нет)
+   * @public
+   */
+  resolve (url) {
+    if (!url) 
+      return;
+
+    return this.routes[url];
+  }
+
+  /**
+   * Добавляет обработчик маршрута.
+   * @param  {String}   path Маршрут
+   * @param  {Function} func Обработчик
+   * @return {Router}
+   * @public
+   */
+  route (path, func) {
+    if (!path || !func) 
+      return;
+
+    // Добавляем начальный слэш, если его нет.
+    if (!path.startsWith('/')) 
+      path = '/' + path;
+
+    // Убираем слэши в конце строки, если они есть.
+    if (path.length > 1 && path.endsWith('/')) 
+      path = path.slice(0, -1);
+
+    this.routes[path] = func;
+
+    return this;
+  }
+}
+
+module.exports = Router;
